e2016e5a4d7a24e46783a81a63c8ffeeecf3bd91
Hassan <muhammadhassanchannel786@gmail.com>
2023-10-31 21:03:41 +0500

    feat: light/dark mode toggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
-import Image from 'next/image'
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div></div>
+    <div>
+      Hello World
+      <Button>CLick Me</Button>
+    </div>
   )
 }
---
+++ src/components/layout/navbar.tsx
@@ -0,0 +1,14 @@
+import { UserButton } from '@clerk/nextjs'
+import React from 'react'
+import { ModeToggle } from '../ui/modeToggle'
+
+const Navbar = () => {
+  return (
+    <div className='w-full h-20 flex justify-end px-10 gap-5 items-center'>
+        <ModeToggle />
+        <UserButton />
+    </div>
+  )
+}
+
+export default Navbar
